183ed5ca424b09f8e90129b66b27c823301f1231
Jess Rascal <me@jessrascal.com>
2024-03-09 18:26:40 +0000

    ESLint: Add Prettier config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,8 @@
 module.exports = {
   root: true,
-  extends: ["@nuxt/eslint-config"],
-  plugins: [],
-  rules: {},
+  extends: ['@nuxt/eslint-config', 'plugin:prettier/recommended', 'prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
 };
