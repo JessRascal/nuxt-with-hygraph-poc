cbbb84de134da176491bf43f4626278409323b45
Jess Rascal <me@jessrascal.com>
2024-03-10 11:42:37 +0000

    Footer: Basic styling

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <footer>&copy; 2024 JessRascal</footer>
+  <footer class="p-4 text-xs bg-primary-950 text-white">
+    &copy; 2024 Concision
+  </footer>
 </template>
-
-<style scoped></style>
