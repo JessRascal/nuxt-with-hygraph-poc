06cd2fe46e9eb5dacf368b85cb2467a51095758f
Jess Rascal <me@jessrascal.com>
2024-03-10 11:43:20 +0000

    AppNav: Basic styling

--- components/AppNav.vue
+++ components/AppNav.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts"></script>
 
 <template>
-  <nav>
-    <NuxtLink to="/">Home</NuxtLink>
-    <NuxtLink to="/articles">Articles</NuxtLink>
+  <nav class="flex gap-x-4 p-4 bg-white border-b border-b-gray-200">
+    <NavItem to="/" label="Home" />
+    <NavItem to="/articles" label="Articles" />
   </nav>
 </template>
 
-<style scoped></style>
+<style scoped>
+.router-link-active,
+.router-link-exact-active {
+  @apply bg-primary-100 rounded;
+}
+</style>
